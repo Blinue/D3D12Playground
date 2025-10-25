f423e8bbce15ca32927f4cddb7b7b950afc62437
Xu <blinue@outlook.com>
2025-10-25 20:49:39 +0800

    Revert "feat: 实现 dcomp 呈现"
    This reverts commit bdd16198635003d5d18e94044042d6a5ec01d77c.

--- src/Presenter.cpp
+++ src/Presenter.cpp
@@ -1,6 +1,7 @@
 #include "pch.h"
 #include "Presenter.h"
 #include "Win32Helper.h"
+#include <dcomp.h>
 #include <dwmapi.h>
 
 Presenter::~Presenter() {
@@ -15,7 +16,6 @@ bool Presenter::Initialize(
 	uint32_t width,
 	uint32_t height
 ) noexcept {
-	_device = device;
 	_commandQueue = commandQueue;
 
 	DXGI_SWAP_CHAIN_DESC1 swapChainDesc = {
@@ -68,7 +68,7 @@ bool Presenter::Initialize(
 	{
 		D3D12_DESCRIPTOR_HEAP_DESC rtvHeapDesc = {
 			.Type = D3D12_DESCRIPTOR_HEAP_TYPE_RTV,
-			.NumDescriptors = (UINT)_renderTargets.size() + 1
+			.NumDescriptors = (UINT)_renderTargets.size()
 		};
 		if (FAILED(device->CreateDescriptorHeap(&rtvHeapDesc, IID_PPV_ARGS(&_rtvHeap)))) {
 			return false;
@@ -84,60 +84,31 @@ bool Presenter::Initialize(
 	if (FAILED(_fenceEvent.create())) {
 		return false;
 	}
-
-	return _LoadSwapChainSizeDependentResources();
+	
+	return _LoadSizeDependentResources(device);
 }
 
 bool Presenter::BeginFrame(
 	winrt::com_ptr<ID3D12Resource>& frameTex,
-	D3D12_RESOURCE_STATES& texState,
 	CD3DX12_CPU_DESCRIPTOR_HANDLE& rtvHandle
 ) noexcept {
-	if (_isDCompPresenting) {
-		if (!_WaitForGpu()) {
-			return false;
-		}
-
-		HRESULT hr = _dcompSurface->BeginDraw(nullptr, IID_PPV_ARGS(&_d3d11Tex), &_drawOffset);
-		if (FAILED(hr)) {
-			return false;
-		}
-		
-		_d3d11DC->Flush();
-
-		frameTex = _dcompRenderTarget;
-		texState = D3D12_RESOURCE_STATE_COPY_SOURCE;
-		rtvHandle = CD3DX12_CPU_DESCRIPTOR_HANDLE(
-			_rtvHeap->GetCPUDescriptorHandleForHeapStart(), 2, _rtvDescriptorSize);
-	} else {
-		if (!_isframeLatencyWaited) {
-			_frameLatencyWaitableObject.wait(1000);
-			_isframeLatencyWaited = true;
-		}
-
-		if (!_WaitForGpu()) {
-			return false;
-		}
-
-		frameTex = _renderTargets[_frameIndex];
-		texState = D3D12_RESOURCE_STATE_PRESENT;
-		rtvHandle = CD3DX12_CPU_DESCRIPTOR_HANDLE(
-			_rtvHeap->GetCPUDescriptorHandleForHeapStart(), _frameIndex, _rtvDescriptorSize);
+	if (!_isframeLatencyWaited) {
+		_frameLatencyWaitableObject.wait(1000);
+		_isframeLatencyWaited = true;
 	}
+
+	if (!_WaitForGpu()) {
+		return false;
+	}
+
+	frameTex = _renderTargets[_frameIndex];
+	rtvHandle = CD3DX12_CPU_DESCRIPTOR_HANDLE(
+		_rtvHeap->GetCPUDescriptorHandleForHeapStart(), _frameIndex, _rtvDescriptorSize);
 
 	return true;
 }
 
 void Presenter::EndFrame(bool waitForGpu) noexcept {
-	if (_isDCompPresenting) {
-		ID3D11Resource* resources[1] = { _wrappedDcompRenderTarget.get() };
-		_d3d11On12Device->AcquireWrappedResources(resources, 1);
-		_d3d11DC->CopySubresourceRegion(_d3d11Tex.get(), 0, _drawOffset.x, _drawOffset.y, 0, _wrappedDcompRenderTarget.get(), 0, nullptr);
-		_dcompSurface->EndDraw();
-		_d3d11On12Device->ReleaseWrappedResources(resources, 1);
-		_d3d11DC->Flush();
-	}
-
 	if (waitForGpu || _isResized) {
 		// 下面两个调用用于减少调整窗口尺寸时的边缘闪烁。
 		// 
@@ -163,84 +134,33 @@ void Presenter::EndFrame(bool waitForGpu) noexcept {
 		_isResized = false;
 	}
 
-	if (_isDCompPresenting) {
-		_dcompDevice->Commit();
-	} else {
-		_swapChain->Present(1, 0);
-		_isframeLatencyWaited = false;
+	_swapChain->Present(1, 0);
+	_isframeLatencyWaited = false;
 
-		_frameIndex = _swapChain->GetCurrentBackBufferIndex();
-
-		if (_isSwitchingToSwapChain) {
-			_isSwitchingToSwapChain = false;
-
-			// 等待交换链呈现新帧
-			_WaitForGpu();
-			_WaitForDwmComposition();
-
-			// 清除 DirectCompostion 内容
-			_dcompVisual->SetContent(nullptr);
-			_dcompDevice->Commit();
-		}
-	}
+	_frameIndex = _swapChain->GetCurrentBackBufferIndex();
 }
 
-bool Presenter::Resize(uint32_t width, uint32_t height, bool onResizing) noexcept {
+bool Presenter::Resize(ID3D12Device5* device, uint32_t width, uint32_t height) noexcept {
 	_isResized = true;
 
-	if (onResizing) {
-		// 切换到 DirectComposition 呈现，失败则回落到交换链
-		_isDCompPresenting = _ResizeDCompVisual(width, height);
-		if (_isDCompPresenting) {
-			D3D12_RESOURCE_DESC texDesc = {
-				.Dimension = D3D12_RESOURCE_DIMENSION_TEXTURE2D,
-				.Width = width,
-				.Height = height,
-				.DepthOrArraySize = 1,
-				.MipLevels = 1,
-				.Format = DXGI_FORMAT_R8G8B8A8_UNORM,
-				.SampleDesc = {
-					.Count = 1
-				},
-				.Flags = D3D12_RESOURCE_FLAG_ALLOW_RENDER_TARGET
-			};
-
-			D3D12_HEAP_PROPERTIES heapProps = {
-				.Type = D3D12_HEAP_TYPE_DEFAULT
-			};
-
-			D3D12_CLEAR_VALUE clearValue{
-				.Format = DXGI_FORMAT_R8G8B8A8_UNORM,
-				.Color = { 0.9f,0.9f,0.8f,1.0f }
-			};
-
-			HRESULT hr = _device->CreateCommittedResource(
-				&heapProps,
-				D3D12_HEAP_FLAG_NONE,
-				&texDesc,
-				D3D12_RESOURCE_STATE_COPY_SOURCE,
-				&clearValue,
-				IID_PPV_ARGS(&_dcompRenderTarget)
-			);
-			if (FAILED(hr)) {
-				return false;
-			}
-
-			_device->CreateRenderTargetView(_dcompRenderTarget.get(), nullptr,
-				CD3DX12_CPU_DESCRIPTOR_HANDLE(_rtvHeap->GetCPUDescriptorHandleForHeapStart(), 2, _rtvDescriptorSize));
-
-			D3D11_RESOURCE_FLAGS flags{};
-			hr = _d3d11On12Device->CreateWrappedResource(_dcompRenderTarget.get(), &flags,
-				D3D12_RESOURCE_STATE_COPY_SOURCE, D3D12_RESOURCE_STATE_COPY_SOURCE, IID_PPV_ARGS(&_wrappedDcompRenderTarget));
-			if (FAILED(hr)) {
-				return false;
-			}
-
-			return true;
-		}
+	if (!_isframeLatencyWaited) {
+		_frameLatencyWaitableObject.wait(1000);
+		_isframeLatencyWaited = true;
 	}
 
-	return _ResizeSwapChain(width, height);
+	_WaitForGpu();
+
+	_renderTargets.fill(nullptr);
+
+	HRESULT hr = _swapChain->ResizeBuffers(0, width, height,
+		DXGI_FORMAT_UNKNOWN, DXGI_SWAP_CHAIN_FLAG_FRAME_LATENCY_WAITABLE_OBJECT);
+	if (FAILED(hr)) {
+		return false;
+	}
+
+	_frameIndex = _swapChain->GetCurrentBackBufferIndex();
+
+	return _LoadSizeDependentResources(device);
 }
 
 bool Presenter::_WaitForGpu() noexcept {
@@ -264,104 +184,14 @@ bool Presenter::_WaitForGpu() noexcept {
 	return true;
 }
 
-bool Presenter::_LoadSwapChainSizeDependentResources() noexcept {
+bool Presenter::_LoadSizeDependentResources(ID3D12Device5* device) noexcept {
 	CD3DX12_CPU_DESCRIPTOR_HANDLE rtvHandle(_rtvHeap->GetCPUDescriptorHandleForHeapStart());
 	for (UINT i = 0; i < (UINT)_renderTargets.size(); ++i) {
 		if (FAILED(_swapChain->GetBuffer(i, IID_PPV_ARGS(&_renderTargets[i])))) {
 			return false;
 		}
-		_device->CreateRenderTargetView(_renderTargets[i].get(), nullptr, rtvHandle);
+		device->CreateRenderTargetView(_renderTargets[i].get(), nullptr, rtvHandle);
 		rtvHandle.Offset(1, _rtvDescriptorSize);
-	}
-
-	return true;
-}
-
-bool Presenter::_ResizeSwapChain(uint32_t width, uint32_t height) noexcept {
-	if (!_isframeLatencyWaited) {
-		_frameLatencyWaitableObject.wait(1000);
-		_isframeLatencyWaited = true;
-	}
-
-	_WaitForGpu();
-
-	_renderTargets.fill(nullptr);
-
-	HRESULT hr = _swapChain->ResizeBuffers(0, width, height,
-		DXGI_FORMAT_UNKNOWN, DXGI_SWAP_CHAIN_FLAG_FRAME_LATENCY_WAITABLE_OBJECT);
-	if (FAILED(hr)) {
-		return false;
-	}
-
-	_frameIndex = _swapChain->GetCurrentBackBufferIndex();
-
-	return _LoadSwapChainSizeDependentResources();
-}
-
-bool Presenter::_ResizeDCompVisual(uint32_t width, uint32_t height) noexcept {
-	if (_dcompSurface) {
-		// 使用 IDCompositionVirtualSurface 而不是 IDCompositionSurface 的原因是
-		// IDCompositionDevice2::CreateSurface 有时相当慢，最坏情况下要几十毫秒。
-		HRESULT hr = _dcompSurface->Resize(width, height);
-		if (FAILED(hr)) {
-			return false;
-		}
-	} else {
-		UINT flags = 0;
-#ifdef _DEBUG
-		flags |= D3D11_CREATE_DEVICE_DEBUG;
-#endif
-		HRESULT hr = D3D11On12CreateDevice(_device, flags, nullptr, 0,
-			(IUnknown**)&_commandQueue, 1, 0, _d3d11Device.put(), _d3d11DC.put(), nullptr);
-		if (FAILED(hr)) {
-			return false;
-		}
-
-		_d3d11Device.try_as(_d3d11On12Device);
-
-		// 初始化 DirectComposition
-		hr = DCompositionCreateDevice3(
-			_d3d11Device.get(), IID_PPV_ARGS(&_dcompDevice));
-		if (FAILED(hr)) {
-			return false;
-		}
-
-		HWND hwndAttach;
-		hr = _swapChain->GetHwnd(&hwndAttach);
-		if (FAILED(hr)) {
-			return false;
-		}
-
-		hr = _dcompDevice->CreateTargetForHwnd(hwndAttach, TRUE, _dcompTarget.put());
-		if (FAILED(hr)) {
-			return false;
-		}
-
-		hr = _dcompDevice->CreateVisual(_dcompVisual.put());
-		if (FAILED(hr)) {
-			return false;
-		}
-
-		hr = _dcompTarget->SetRoot(_dcompVisual.get());
-		if (FAILED(hr)) {
-			return false;
-		}
-
-		hr = _dcompDevice->CreateVirtualSurface(
-			width,
-			height,
-			DXGI_FORMAT_R8G8B8A8_UNORM,
-			DXGI_ALPHA_MODE_IGNORE,
-			_dcompSurface.put()
-		);
-		if (FAILED(hr)) {
-			return false;
-		}
-	}
-
-	HRESULT hr = _dcompVisual->SetContent(_dcompSurface.get());
-	if (FAILED(hr)) {
-		return false;
 	}
 
 	return true;
