93c1e01089099c46712ce7f0c26538d46310677c
Xu <blinue@outlook.com>
2025-11-13 20:39:40 +0800

    feat: 修正 P3 三原色

--- src/shaders/AdvancedColor_PS.hlsl
+++ src/shaders/AdvancedColor_PS.hlsl
@@ -3,9 +3,9 @@ cbuffer RootConstants : register(b0) {
 };
 
 float4 main(noperspective float2 uv : TEXCOORD) : SV_Target {
-	const float3 p3_r = { 1.052922, -0.036065, -0.016857 };
-	const float3 p3_g = { -0.304578, 1.411079, -0.106501 };
-	const float3 p3_b = { 0, 0, 1 };
+	const float3 p3_r = { 1.22494, -0.042057, -0.019638 };
+	const float3 p3_g = { -0.22494, 1.042057, -0.078636 };
+	const float3 p3_b = { 0, 0, 1.098274 };
 	
 	float3 c1 = lerp(p3_r, (p3_r + p3_b) / 2, uv.x);
 	float3 c2 = lerp(p3_g, p3_b, uv.x);
