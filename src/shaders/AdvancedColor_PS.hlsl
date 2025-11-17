fddc895d9c3c7ec00cfc0a21fe45a29ee083ceb2
Xu <xu.liu@bonsmile.com>
2025-11-17 10:40:02 +0800

    feat: 交换红色和蓝色位置使 SDR 和 WCG 差别更明显

--- src/shaders/AdvancedColor_PS.hlsl
+++ src/shaders/AdvancedColor_PS.hlsl
@@ -7,7 +7,7 @@ float4 main(noperspective float2 uv : TEXCOORD) : SV_Target {
 	const float3 p3_g = { -0.22494, 1.042057, -0.078636 };
 	const float3 p3_b = { 0, 0, 1.098274 };
 	
-	float3 c1 = lerp(p3_r, (p3_r + p3_b) / 2, uv.x);
-	float3 c2 = lerp(p3_g, p3_b, uv.x);
+	float3 c1 = lerp(p3_b, (p3_r + p3_b) / 2, uv.x);
+	float3 c2 = lerp(p3_g, p3_r, uv.x);
 	return float4(lerp(c1, c2, uv.y) * boost, 1);
 }
