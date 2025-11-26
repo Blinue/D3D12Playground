aca19403b5b4fd8dd350779ac80b0a69d7067106
Xu <xu.liu@bonsmile.com>
2025-11-26 12:52:51 +0800

    chore: Debug 配置下使用 Agility SDK 辅助调试

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,12 @@
 #include "pch.h"
 #include "MainWindow.h"
 
+// Debug 配置下使用 Agility SDK 辅助调试
+#ifdef _DEBUG
+extern "C" { __declspec(dllexport) extern const UINT D3D12SDKVersion = 618; }
+extern "C" { __declspec(dllexport) extern const char* D3D12SDKPath = ".\\D3D12\\"; }
+#endif
+
 int APIENTRY wWinMain(
 	_In_ HINSTANCE /*hInstance*/,
 	_In_opt_ HINSTANCE /*hPrevInstance*/,
